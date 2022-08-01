63c839938915df3ba29975029daf5275b46daf47
PaulaPolacco <paulapolacco@gmail.com>
2022-08-01 17:35:34 -0300

    implementacion de fetch y productos en json

--- index.js
+++ index.js
@@ -1,38 +1,7 @@
+let productosaux; // para que quede consistente la parte de destacados
 
-class Producto{
-    constructor(id,nombre, precio, tipo, stock, obs, img){
-        this.id = parseInt(id);     //luego autogenerar
-        this.nombre = nombre;
-        this.precio = parseFloat(precio);
-        this.tipo = tipo.toUpperCase(); // puede ser general, customizado, personalizado
-        this.stock = parseInt(stock);
-        this.observado = parseInt(obs); // para destacados
-        this.img = img;
-    }
-
-}
-
-function formaPago(){
-    let pago;
-    do {
-        pago = parseInt(prompt("ingrese forma de pago: \n 1-Efectivo(-10%) \n 2-Transferencia(-5%) \n 3-crédito(+7%)"));
-    } while (pago < 1 || pago > 3);
-    console.log(pago);
-    return pago;
-}
-
-function calcularPrecioFinal(tipo,valor){
-    switch (tipo){
-        case 1: valor = valor*0.9; break;
-        case 2: valor = valor*0.95; break;
-        default: valor = valor+(valor*0.7); break;
-    }
-    alert(`El precio final de la compra es ${valor}`);
-}
-
-function actualizarDestacados(){
-    console.log(productos.sort((a,b) => b.observado - a.observado));
-    let destacados = productos.sort((a,b) => b.observado - a.observado).slice(0,3);
+const actualizarDestacados = async() => {
+    let destacados = productosaux.sort((a,b) => b.obs - a.obs).slice(0,3);
     console.log(destacados);
     let i=1;
     for (const destacado of destacados){
@@ -41,12 +10,10 @@ function actualizarDestacados(){
         i+=1;
     }
 }
-
-function verMas(id) {
+const verMas = async(id) => {
     var dots = document.getElementById("dots");
     var moreText = document.getElementById(`more${id}`);
     var btnText = document.getElementById(`moreBtn${id}`);
-  
     if (dots.style.display === "none") {
         dots.style.display = "inline";
         btnText.innerHTML = "+ Descripción";
@@ -56,26 +23,24 @@ function verMas(id) {
         btnText.innerHTML = "- Descripción";
         moreText.style.display = "block";
         // incremento el flag observado para que figure en Destacados
-        let prod = productos.find(prod => prod.id === id)
-        prod.observado += 1;
-        console.log(`Producto: ${prod.nombre} - observado: ${prod.observado}`);
+         let prod = productosaux.find(prod => prod.id === id)
+        prod.obs += 1;
+        console.log(`Producto: ${prod.nombre} - observado: ${prod.obs}`); 
+        //await actualizarObsProducto(id);
         actualizarDestacados();
     }
-
 } 
-  
-function verCar(){
+const verCar = () => {
     let detalle = "<ul>";
     let total = 0;
     for(let i = 0; i < localStorage.length; i++){
         let key = localStorage.key(i);
         let producto =  JSON.parse(localStorage.getItem(key));
         total += parseInt(producto.precio)*parseInt(producto.cantidad);
-        detalle += `<li> ${producto.nombre} $: ${producto.precio} - ${producto.cantidad} unidades </li>`;//localStorage.getItem(key);
+        detalle += `<li> ${producto.nombre} $: ${producto.precio} - ${producto.cantidad} unidades </li>`;
       }
     detalle += "</ul>";
-    //  alert("Detalle: \n"+detalle + "\n Total = "+ total);
-      Swal.fire({
+    Swal.fire({
         title: '<strong>Detalle de compra:</strong>',
         icon: 'info',
         html:
@@ -84,24 +49,17 @@ function verCar(){
         showCancelButton: true,
         focusConfirm: false,
         confirmButtonText:
-          '<i class="fa fa-thumbs-up"></i> Pagar',
+            '<i class="fa fa-thumbs-up"></i> Pagar',
         confirmButtonAriaLabel: 'Thumbs up, great!',
         cancelButtonText:
-          '<i class="fa fa-thumbs-down"></i> Eliminar carrito',
+            '<i class="fa fa-thumbs-down"></i> Eliminar carrito',
         cancelButtonAriaLabel: 'Thumbs down'
-      })
+    })
 }
-
-//calcular pagos de productos
-let continua = true;
-let tipoPago;
-let monto;
 const addCar = (clave, nombre, precio) => {
     let producto = JSON.parse(localStorage.getItem(clave));
     let cantidad = 1;
-    if(producto){
-       cantidad = producto.cantidad + 1;
-    }
+    producto && (cantidad = producto.cantidad + 1);
     localStorage.setItem(clave, JSON.stringify({nombre, precio, cantidad}));
     Swal.fire({
         position: 'top-end',
@@ -111,44 +69,38 @@ const addCar = (clave, nombre, precio) => {
         timer: 1500
       });
 };
-// crear los productos a vender y listarlos en la galeria
-const productos = [];
-productos.push(new Producto(1,"Llavero porta celu",250, "general",20,0,"images/galeria/llaveros.png"));
-productos.push(new Producto(2,"Maceta",600, "general",5,0,"images/galeria/macetas.jpg"));
-productos.push(new Producto(3,"Soporte para Celular",500, "general",15,0,"images/galeria/porta_celus.png"));
-productos.push(new Producto(4,"Soporte para Notebook",1000, "general",3,0,"images/galeria/soporte_notebook.jpg"));
-productos.push(new Producto(5,"Aspa Ventilador",300, "personalizado",4,0,"images/galeria/aspa_ventilador.jpg"));
-productos.push(new Producto(6,"Juego de Ajedrez",2500, "general",1,0,"images/galeria/ajedrez.png"));
-productos.push(new Producto(7,"Porta Joystick x 2",800, "general",10,0,"images/galeria/porta_joystick.png"));
-productos.push(new Producto(8,"Soporte para Auriculares",700, "general",6,0,"images/galeria/soporte_auriculares.jpg"));
-productos.push(new Producto(9,"Florero",700, "general",8,0,"images/galeria/florero.jpg"));
+// solicita los productos a productService para listarlos en la galeria
 
-listado = document.getElementById('listaProductos');
-for(const producto of productos){
-    
-    let div = document.createElement("div");
-    let storage = `{${producto.id},${producto.nombre},${producto.precio}`;
-    console.log(producto.nombre)
-    
-    div.innerHTML =  `<img src="${producto.img}" alt = "${producto.nombre}" class="imagenesGaleria">
-                      <p>${producto.nombre}</p>
-                      <p>Precio: $${producto.precio} <span id="dots">...</span></p>
-                      <ol class = "more" id="more${producto.id}">
-                        <li>Tamaño 0x0 cm</li>
-                        <li>Tiempo de impresión: 0 min</li>
-                        <li>Cantidad de filamento: 0 gr</li>
-                        <li>Colores disponibles<ul id="listaproductos_colores">
-                            <li class="listaproductos_cuadrado green"> </li>
-                            <li class="listaproductos_cuadrado fuchsia"> </li>
-                            <li class="listaproductos_cuadrado"> </li>
-                            <li class="listaproductos_cuadrado black"> </li>
-                        </ul></li>
-                       </ol>
-                       <div>
-                            <button onclick="verMas(${producto.id})" id="moreBtn${producto.id}">+ Descripción</button>
-                            <button onclick="addCar(${producto.id}, '${producto.nombre}' ,${producto.precio})" id="buy${producto.id}">Comprar</button>
-                       </div>`;
-    listado.append(div);
+const cargarProductos = async () => {
+    let productos = await traerProductos();
+    productosaux = productos;
+    listado = document.getElementById('listaProductos');
+    for(const producto of productos){
+        let div = document.createElement("div");
+        div.innerHTML =  `<img src="${producto.img}" alt = "${producto.nombre}" class="imagenesGaleria">
+                        <p>${producto.nombre}</p>
+                        <p>Precio: $${producto.precio} <span id="dots">...</span></p>
+                        <ol class = "more" id="more${producto.id}">
+                            <li>Tamaño 0x0 cm</li>
+                            <li>Tiempo de impresión: 0 min</li>
+                            <li>Cantidad de filamento: 0 gr</li>
+                            <li>Colores disponibles<ul id="listaproductos_colores">
+                                <li class="listaproductos_cuadrado green"> </li>
+                                <li class="listaproductos_cuadrado fuchsia"> </li>
+                                <li class="listaproductos_cuadrado"> </li>
+                                <li class="listaproductos_cuadrado black"> </li>
+                            </ul></li>
+                        </ol>
+                        <div>
+                                <button onclick="verMas(${producto.id})" id="moreBtn${producto.id}">+ Descripción</button>
+                                <button onclick="addCar(${producto.id}, '${producto.nombre}' ,${producto.precio})" id="buy${producto.id}">Comprar</button>
+                        </div>`;
+        listado.append(div);
+    }
+    actualizarDestacados(); 
 }
+ 
+cargarProductos()
 
-actualizarDestacados();
+
+
---
+++ services/productService.js
@@ -0,0 +1,42 @@
+  
+const traerProductos = async () => {
+    try {
+        const resp = await fetch("https://cr3adly.000webhostapp.com/productos.json",{
+            method: "GET",
+            headers: {
+            }});
+        const data = await resp.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const actualizarObsProducto = async (id) => {
+    try {
+        const resp = await fetch("https://cr3adly.000webhostapp.com/productos.json",{
+            method: "GET",
+            headers: {
+            }});
+        const data = await resp.json();
+        prod = data.find(prod => prod.id === id);
+        prod.obs +=1;
+        console.log(prod.obs);
+      //  data.observado += 1;
+      //  return data;
+        await postProducto(prod);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const postProducto = async (prod) => {
+    fetch("../mock/productos.json", {
+        method: "POST",
+        body: JSON.stringify(prod),
+        headers: {'Accept': 'application/json',
+        "Content-type": "application/json; charset=UTF-8",
+        },
+        }).then(response => response.json())
+        .then(data => console.log(data));
+}
